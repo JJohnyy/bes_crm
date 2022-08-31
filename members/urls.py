94c64555dc94ac2bd09e768afc319fd5a80bf7a5
JJohnyy <jan.jasa@email.cz>
2022-08-31 13:47:12 +0000

    created dashboard view and template

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, DashboardView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('dashoard/', DashboardView.as_view(), name='dashboard'),
 ]
