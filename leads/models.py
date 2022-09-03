2ad7723f857dfa93538cce5a113a7ea7816dea48
JJohnyy <jan.jasa@email.cz>
2022-09-03 07:07:06 +0000

    created a lead model and made migrations

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
