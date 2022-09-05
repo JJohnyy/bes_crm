267a25556f7dfd804d569fdb412c4e629039c512
JJohnyy <jan.jasa@email.cz>
2022-09-05 19:11:01 +0000

    lead delete css and navabr css

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import LeadsListView, LeadCreateView, LeadUpdateView
+from .views import (
+    LeadsListView,
+    LeadCreateView,
+    LeadUpdateView,
+    LeadDeleteView
+    )
 
 
 urlpatterns = [
     path('', LeadsListView.as_view(), name="leads"),
     path('create/', LeadCreateView.as_view(), name="leads_create"),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name="leads_update"),
-    path('<int:pk>/delete/', LeadUpdateView.as_view(), name="leads_delete"),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name="leads_delete"),
 ]
